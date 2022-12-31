72fa4ad29a33c2039361203527563c709853394a
michael <mike.michaelmilstead@gmail.com>
2022-12-31 14:26:57 -0800

    init

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import { ChatGPTAPIBrowser } from "chatgpt";
+import promptsync from "prompt-sync";
+
+async function example() {
+  // use puppeteer to bypass cloudflare (headful because of captchas)
+  const api = new ChatGPTAPIBrowser({
+    email: process.env.OPENAI_EMAIL!,
+    password: process.env.OPENAI_PASSWORD!,
+  });
+
+  await api.initSession();
+
+  const prompt = promptsync();
+
+  while (true) {
+    const gptPrompt = prompt("message to chatgpt: ");
+    const result = await api.sendMessage(gptPrompt);
+    console.log(result.response);
+  }
+}
+
+console.log("starting");
+await example();
